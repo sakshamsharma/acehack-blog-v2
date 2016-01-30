92092676b0ba4ae640a9dbfc4dd0a7e87baf5413
Saksham Sharma <saksham0808@gmail.com>
2016-01-31 02:34:15 +0530

    Update contact.

--- site.hs
+++ site.hs
@@ -19,7 +19,7 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.md", "contact.markdown"]) $ do
+    match (fromList ["about.md"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
